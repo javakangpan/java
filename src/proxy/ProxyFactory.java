6ece441a5ef2a46ccc1d98057a05dc757b032ed6
kangpan <13728897992@163.com>
2018-08-18 18:12:42 +0800

    add

--- src/proxy/ProxyFactory.java
+++ src/proxy/ProxyFactory.java
@@ -3,7 +3,10 @@ package proxy;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-
+//static Object newProxyInstance(ClassLoader loader, Class<?>[] interfaces,InvocationHandler h )
+// ClassLoader loader 指定当前目标对象使用类加载器,获取加载器的方法是固定的
+// Class<?>[] interfaces 目标对象实现的接口的类型,使用泛型方式确认类型	
+//InvocationHandler h 事件处理,执行目标对象的方法时,会触发事件处理器的方法,会把当前执行目标对象的方法作为参数传入
 public class ProxyFactory {
 	private Object target;
 	public ProxyFactory(Object target) {
